9170e0f4e2b29e3bd6f4e147561dd7be92cf021c
chenzhibo <624176391@qq.com>
2017-03-16 18:20:04 +0800

    测试log4j

--- src/main/java/cn/springmvc/controller/UserComtroller.java
+++ src/main/java/cn/springmvc/controller/UserComtroller.java
@@ -1,13 +1,18 @@
 package cn.springmvc.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 @RequestMapping("/")
 public class UserComtroller {
+	Logger logger =  Logger.getLogger(UserComtroller.class);
 	@RequestMapping("index")
     public String index(){
+		logger.error("test");
+		
         return "index";
     }
 }
--- src/main/java/cn/springmvc/service/impl/UserServiceImpl.java
+++ src/main/java/cn/springmvc/service/impl/UserServiceImpl.java
@@ -2,17 +2,20 @@ package cn.springmvc.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import cn.springmvc.dao.UserDAO;
 import cn.springmvc.model.User;
 import cn.springmvc.service.UserService;
 
 @Service
+@Transactional
 public class UserServiceImpl implements UserService {
 	@Autowired
 	private UserDAO userDAO;
 
 	public int insertUser(User user) {
-		return userDAO.insertUser(user);
+		int count = userDAO.insertUser(user);
+		return count;
 	}
 }
